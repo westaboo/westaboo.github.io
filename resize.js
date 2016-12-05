eb2618435e80d80842173d6d9f9cd00f8db18198
Zeeshan Hanif <zhanif@uci.edu>
2016-12-04 16:49:40 -0800

    cleaned up file directory/added resume

--- resize.js
+++ resize.js
@@ -3,8 +3,3 @@ intro.style.height = intro.clientHeight.toString() + 'px';
 
 var introName = document.getElementById('intro-name');
 introName.style.marginTop = (intro.clientHeight * .18).toString() + 'px';
-
-var introLink1 = document.getElementById('intro-link-1');
-var introLink2 = document.getElementById('intro-link-2');
-introLink1.style.marginTop = (intro.clientHeight * .075).toString() + 'px';
-introLink2.style.marginTop = (intro.clientHeight * .075).toString() + 'px';
