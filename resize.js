8528ee7fb8b807d324506033a18069277d626ab2
westaboo <zhanif@uci.edu>
2017-02-01 20:21:16 -0800

    added timeline filtering

--- resize.js
+++ resize.js
@@ -3,3 +3,15 @@ intro.style.height = intro.clientHeight.toString() + 'px';
 
 var introName = document.getElementById('intro-name');
 introName.style.marginTop = (intro.clientHeight * .18).toString() + 'px';
+
+function hideProjects() {
+  event.preventDefault();
+  document.getElementById("projects").style.display = "None";
+  document.getElementById("work").style.display = "";
+};
+
+function hideWork() {
+  event.preventDefault();
+  document.getElementById("work").style.display = "None";
+  document.getElementById("projects").style.display = "";
+};
