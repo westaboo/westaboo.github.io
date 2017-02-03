ac0f07a4278ceda17284f07c6338a02a719b9f1b
westaboo <zhanif@uci.edu>
2017-02-03 15:31:11 -0800

    added bold style to filter/clouds

--- resize.js
+++ resize.js
@@ -8,10 +8,14 @@ function hideProjects() {
   event.preventDefault();
   document.getElementById("projects").style.display = "None";
   document.getElementById("work").style.display = "";
+  document.getElementById("filter-projects").style.fontWeight = "";
+  document.getElementById("filter-work").style.fontWeight = "bold";
 };
 
 function hideWork() {
   event.preventDefault();
   document.getElementById("work").style.display = "None";
   document.getElementById("projects").style.display = "";
+  document.getElementById("filter-work").style.fontWeight = "";
+  document.getElementById("filter-projects").style.fontWeight = "bold";
 };
