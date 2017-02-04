cc31896b06ce57ae1c4eec5fa842d3a43e109c5b
westaboo <zhanif@uci.edu>
2017-02-03 19:30:55 -0800

    added variability in clouds

--- resize.js
+++ resize.js
@@ -4,6 +4,8 @@ intro.style.height = intro.clientHeight.toString() + 'px';
 var introName = document.getElementById('intro-name');
 introName.style.marginTop = (intro.clientHeight * .18).toString() + 'px';
 
+generateClouds(3);
+
 function hideProjects() {
   event.preventDefault();
   document.getElementById("projects").style.display = "None";
@@ -19,3 +21,24 @@ function hideWork() {
   document.getElementById("filter-work").style.fontWeight = "";
   document.getElementById("filter-projects").style.fontWeight = "bold";
 };
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateClouds(n) {
+  if(getRandom(0,1)) {l = ['left', 'right'];}
+  else {l = ['right', 'left'];}
+
+  var cloud = document.getElementById('clouds');
+  for(var i = 0; i < n; ++i) {
+    var dir = l[i%2];
+    var size = getRandom(150, 200);
+    var speed = getRandom(1,2);
+    var height = 0;
+    var el = '<marquee behavior = "scroll" direction = "' + dir +
+      '" scrollamount = "' + speed.toString() + '"><img src = "cloud.png" style = "width: ' +
+      size.toString() + 'px; opacity: 0.2;"/> </marquee>';
+    cloud.innerHTML += el;
+  }
+}
